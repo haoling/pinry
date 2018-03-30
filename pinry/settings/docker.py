2c10884eb85da0db1b40c784c3e091104fba594b
Haoling <maya@fei-yen.jp>
2018-03-30 13:53:49 +0900

    my settings

--- pinry/settings/docker.py
+++ pinry/settings/docker.py
@@ -15,10 +15,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/data/production.db',
     }
 }
